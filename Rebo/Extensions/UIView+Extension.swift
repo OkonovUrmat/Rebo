1c58a0df74cfb909671f237f094744283c81aa40
OkonovUrmat <okonov.okonov@gmail.com>
2022-02-03 23:31:38 +0600

    Create size parameter to set width and height of the element

--- Rebo/Extensions/UIView+Extension.swift
+++ Rebo/Extensions/UIView+Extension.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, bottom: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
+    func anchor(top: NSLayoutYAxisAnchor, bottom: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         translatesAutoresizingMaskIntoConstraints = false
         
         topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
         bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
         leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
         trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true 
+        }
     }
 }
