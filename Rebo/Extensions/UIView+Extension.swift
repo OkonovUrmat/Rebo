d1eeaab440e1e8beca933d42f01c03c84f4328b6
OkonovUrmat <okonov.okonov@gmail.com>
2022-02-03 23:15:41 +0600

    Create extension for UIView

---
+++ Rebo/Extensions/UIView+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIView+Extension.swift
+//  Rebo
+//
+//  Created by Urmat on 3/2/22.
+//
+
+import UIKit
+
+class UIView_Extension: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
